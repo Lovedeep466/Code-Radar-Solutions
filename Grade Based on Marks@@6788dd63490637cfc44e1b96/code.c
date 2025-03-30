2629c900645f026f0e248c89d75d62b8ff4b27bf
Lovedeep <lovedeeplovedeep537537@gmail.com>
2025-03-31 01:13:28 +0530

    Solution to Grade Based on Marks

--- Grade Based on Marks@@6788dd63490637cfc44e1b96/code.c
+++ Grade Based on Marks@@6788dd63490637cfc44e1b96/code.c
@@ -13,10 +13,10 @@ int main()
         printf("C");
     }
     else if(marks>=60){
-        printf("D")
+        printf("D");
     }
     else{
-        printf("F")
+        printf("F");
     }
     return 0;
 }
